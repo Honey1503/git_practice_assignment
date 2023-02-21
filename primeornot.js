6de98abd9adb0ad39d3d9c0f3dc42c602de39a7b
Honey1503 <simranshandge1998@gmail.com>
2023-02-21 22:13:10 +0530

    corrected the code

---
+++ primeornot.js
@@ -0,0 +1,17 @@
+let num=3;
+let count=0;
+    
+        for(let i=1;i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                count++;
+            }
+        }
+    if(count==2)    {
+        console.log("It's a prime number");
+    }
+    else
+    {
+        console.log("It's not a prime number");
+    }
